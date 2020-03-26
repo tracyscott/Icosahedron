08cb059f8bb9bb7806e28368c0ad2610350ba3cb
Tracy Scott <tracy.scott@gmail.com>
2020-03-25 22:11:27 -0700

    Add debugging caps option to BarSelector.

--- src/main/java/noomechanism/icosahedron/patterns/BarSelector.java
+++ src/main/java/noomechanism/icosahedron/patterns/BarSelector.java
@@ -4,6 +4,7 @@ import heronarts.lx.LX;
 import heronarts.lx.LXPattern;
 import heronarts.lx.color.LXColor;
 import heronarts.lx.model.LXPoint;
+import heronarts.lx.parameter.BooleanParameter;
 import heronarts.lx.parameter.DiscreteParameter;
 import noomechanism.icosahedron.IcosahedronModel;
 import noomechanism.icosahedron.LightBar;
@@ -11,18 +12,29 @@ import noomechanism.icosahedron.LightBar;
 public class BarSelector extends LXPattern {
 
   DiscreteParameter barNum = new DiscreteParameter("BarNum", 0, 0, IcosahedronModel.NUM_LIGHT_BARS);
+  BooleanParameter caps = new BooleanParameter("caps", false).setDescription("Debugging caps");
 
   public BarSelector(LX lx) {
     super(lx);
     addParameter(barNum);
+    addParameter(caps);
   }
 
   @Override
   public void run(double deltaMs) {
     for (LightBar lb : IcosahedronModel.lightBars) {
       if (lb.barNum == barNum.getValuei()) {
+        int ptNum = 0;
         for (LXPoint point: lb.points) {
-          colors[point.index] = LXColor.rgba(255, 255, 255, 255);
+          if (caps.getValueb() && ptNum == 0)
+            colors[point.index] = LXColor.rgba(255, 0, 0, 255);
+          else if (caps.getValueb() && (ptNum == 1 || ptNum == lb.points.size() - 2))
+            colors[point.index] = LXColor.rgba(0, 0, 255, 255);
+          else if (caps.getValueb() && (ptNum == lb.points.size() - 1))
+            colors[point.index] = LXColor.rgba(0, 255, 0, 255);
+          else
+            colors[point.index] = LXColor.rgba(255, 255, 255, 255);
+          ptNum++;
         }
       } else {
         for (LXPoint point: lb.points) {
