6ec6a72f90ba9146d306e5dcf068d3c94a0e4e85
Tracy Scott <tracy.scott@gmail.com>
2020-05-01 13:21:09 -0700

    Update Starlight to support adjustable star size.

--- src/main/java/noomechanism/icosahedron/patterns/Starlight.java
+++ src/main/java/noomechanism/icosahedron/patterns/Starlight.java
@@ -9,6 +9,8 @@ import heronarts.lx.modulator.LXModulator;
 import heronarts.lx.modulator.SinLFO;
 import heronarts.lx.parameter.CompoundParameter;
 import heronarts.lx.parameter.FunctionalParameter;
+import noomechanism.icosahedron.IcosahedronModel;
+import noomechanism.icosahedron.LightBar;
 
 import static heronarts.lx.LXUtils.constrain;
 import static heronarts.lx.LXUtils.random;
@@ -17,6 +19,7 @@ public class Starlight extends LXPattern {
 
   public final CompoundParameter speed = new CompoundParameter("Speed", 1, 2, .5);
   public final CompoundParameter base = new CompoundParameter("Base", -10, -20, 100);
+  public final CompoundParameter size = new CompoundParameter("Size", 5f, 1f, 150f);
 
   public final LXModulator[] brt = new LXModulator[50];
   private final int[] map1 = new int[model.size];
@@ -38,13 +41,21 @@ public class Starlight extends LXPattern {
     }
     addParameter("speed", this.speed);
     addParameter("base", this.base);
+    addParameter("size", this.size);
   }
 
   public void run(double deltaMs) {
-    for (LXPoint p : model.points) {
-      int i = p.index;
-      float brt = this.brt[this.map1[i]].getValuef() + this.brt[this.map2[i]].getValuef();
-      colors[i] = LXColor.gray(constrain(.5*brt, 0, 100));
+    int starWidth = (int)size.getValuef();
+
+    int i = 0;
+    int starNum = 0;
+    for (LightBar lb : IcosahedronModel.lightBars) {
+      for (LXPoint pt : lb.points) {
+        starNum = i / starWidth;
+        float brt = this.brt[this.map1[starNum]].getValuef() + this.brt[this.map2[starNum]].getValuef();
+        colors[pt.index] = LXColor.gray(constrain(.5*brt, 0, 100));
+        i++;
+      }
     }
   }
 }
